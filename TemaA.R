7240701ff0a6c7c68befce7ff7a4addd8b175c96
comshiro <83612463+comshiro@users.noreply.github.com>
2024-06-04 10:34:47 +0300

    Add files via upload

--- TemaA.R
+++ TemaA.R
@@ -30,54 +30,52 @@ plot(3, 0.5, 10, 2, 5)
 #c)
 poisson = function(lambda){
 threshold = 1 - 0.000001
-prob=0
-k0 = -1
-while (prob <= threshold) {
-  k0 = k0 + 1
-  prob = prob + dpois(k0, lambda)
+k0 = 0
+while (ppois(k0,lambda) <= threshold) {
+  k0 = k0 + 1;
 }
 print("Cea mai mică valoare a lui k0 pentru care P(Y <= k0) > 1 - 10^-6 este:")
-k0
+return(k0)
 }
 
-poisson(3)
+print(poisson(3))
+
 
 #A2
 #a)
 note_PS = function(file_path){
-data = read.table(file_path, header = TRUE)
-sample_P = data[['P']]
-sample_S = data[['S']]
-
-frequencies_P = as.vector(table(sample_P))
-frequencies_relative_P = frequencies_P / sum(frequencies_P)
-
-frequencies_S = as.vector(table(sample_S))
-frequencies_relative_S = frequencies_S / sum(frequencies_S)
+  data = read.table(file_path, header = TRUE, sep =",")
+  sample_P = data[['P']]
+  sample_S = data[['S']]
   
-mean_P = mean(sample_P)
-mean_S = mean(sample_S)
-
-result = list(
-  frequencies_P = frequencies_P,
-  frequencies_relative_P = frequencies_relative_P,
-  frequencies_S = frequencies_S,
-  frequencies_relative_S = frequencies_relative_S,
-  mean_P = mean_P,
-  mean_S = mean_S
-)
+  frecvente_P_abs = table(sample_P)
+  frecvente_S_abs = table(sample_S)
+  
+  frecvente_P_rel = as.vector(frecvente_P_abs) / length(sample_P)
+  frecvente_S_rel = as.vector(frecvente_S_abs) / length(sample_S)
+  
+  mean_P = mean(sample_P)
+  mean_S = mean(sample_S)
+  
+  result = list(
+    frecvente_P_abs = frecvente_P_abs,
+    frecvente_P_rel = frecvente_P_rel,
+    frecvente_S_abs = frecvente_S_abs,
+    frecvente_S_rel = frecvente_S_rel,
+    mean_P = mean_P,
+    mean_S = mean_S
+  )
 }
 
-result = note_PS("E:\\PS\\note_PS")
-
+result = note_PS("note_PS.csv")
 
 print("Frecvențele absolute ale eșantioanelor:")
-print(result$frequencies_P)
-print(result$frequencies_S)
+print(result$frecvente_P_abs)
+print(result$frecvente_S_abs)
 
 print("Frecvențele relative ale eșantioanelor:")
-print(result$frequencies_relative_P)
-print(result$frequencies_relative_S)
+print(result$frecvente_P_rel)
+print(result$frecvente_S_rel)
 
 print("Media eșantionului 'P':")
 print(result$mean_P)
@@ -87,17 +85,16 @@ print(result$mean_S)
 
 #b)
 elim_aberante = function(file_path, sample_name) {
-  data = read.table(file_path, header = TRUE)
-  
-  sample = data[[sample_name]]
+  data = read.table(file_path, header = TRUE, sep =",")
+  sample = as.numeric(data[[sample_name]])
   
   Q1 = quantile(sample, 0.25)
   Q3 = quantile(sample, 0.75)
   IQR = Q3 - Q1
-  lower_limit = Q1 - 1.5 * IQR
-  upper_limit = Q3 + 1.5 * IQR
+  low = Q1 - 1.5 * IQR
+  up = Q3 + 1.5 * IQR
   
-  corrected = sample[sample >= lower_limit & sample <= upper_limit]
+  corrected = sample[sample >= low & sample <= up]
   
   breaks = seq(1, 10, by = 1)
   hist(corrected, breaks = breaks, main = paste("Distribuția frecvențelor pentru", sample_name), xlab = sample_name, ylab = "Frecvență")
@@ -105,4 +102,5 @@ elim_aberante = function(file_path, sample_name) {
   return(corrected)
 }
 
-corrected_P = elim_aberante("E:\\PS\\note_PS", "P")
+corrected_P = elim_aberante("note_PS.csv", 'P')
+corrected_S = elim_aberante("note_PS.csv", 'S')
